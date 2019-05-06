79d7ca093b4e99622d13b3d93240e99fbd0d5a77
Oleksandr Martinyuk <alexander.martinyuk@gmail.com>
2019-05-07 01:43:18 +0300

    Added DateTimeService

---
+++ Services/DateTimeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VehiclesAPI.Services
+{
+    public class DateTimeService
+    {
+        public virtual DateTime Now
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+    }
+}
