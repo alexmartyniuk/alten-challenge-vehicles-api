ce394d5a175d0dee08d37797a04189095f5580e3
Oleksandr.Martyniuk <alexander.martinyuk@gmail.com>
2019-05-07 16:06:04 +0300

    Added Application Insights support

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace VehiclesAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
